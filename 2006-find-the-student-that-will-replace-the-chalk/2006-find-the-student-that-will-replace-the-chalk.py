5013ec32bef66c99c5362b738558bf9887faab10
Avisiktaa <168320879+Avisiktaa@users.noreply.github.com>
2024-09-02 20:58:10 +0530

    Time: 593 ms (18.87%), Space: 30.6 MB (19.92%) - LeetHub

---
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.py
@@ -0,0 +1,12 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        sums=0
+        for c in chalk:
+            sums+=c
+        k=k%sums
+        for i in range(0,len(chalk)):
+            if k<chalk[i]:
+                return i
+            k-=chalk[i]
+        return 0
+            
