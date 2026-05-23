8ceee230bf6bf2c2b9a44ddd58096aed4e5a4ca8
Avisikta Nath <168320879+Avisiktaa@users.noreply.github.com>
2026-05-23 22:16:03 +0530

    Time: 0 ms (100%), Space: 43 MB (62.78%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -2,49 +2,15 @@ class Solution {
     public boolean check(int[] nums) {
         int n = nums.length;
         if (n <= 2) return true;
-
-        int pivot = findMinIndex(nums);
-
-        int idx = pivot;
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[idx] > nums[(idx + 1) % n]) {
-                return false;
-            }
-            idx = (idx + 1) % n;
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>nums[(i+1)%n])
+            cnt++;
         }
-        return true;
-    }
+        if(cnt>1)
+        return false;
 
-    private int findMinIndex(int[] nums) {
-        int n = nums.length;
-        int min = nums[0];
-        for (int i = 1; i < n; i++) {
-            if (nums[i] < min) {
-                min = nums[i];
-            }
-        }
-
-        // choose the correct occurrence of min
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == min) {
-                // check if starting here yields sorted order
-                if (isValidStart(nums, i)) {
-                    return i;
-                }
-            }
-        }
-        return 0; // fallback
-    }
-
-    private boolean isValidStart(int[] nums, int start) {
-        int n = nums.length;
-        int idx = start;
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[idx] > nums[(idx + 1) % n]) {
-                return false;
-            }
-            idx = (idx + 1) % n;
-        }
         return true;
     }
 }
