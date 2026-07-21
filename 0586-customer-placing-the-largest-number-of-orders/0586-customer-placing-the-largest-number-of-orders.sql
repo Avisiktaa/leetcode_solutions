91af796fd14f3ee6e1e4626497959bd71b20db2a
Avisikta Nath <168320879+Avisiktaa@users.noreply.github.com>
2026-07-21 11:22:25 +0530

    Time: 592 ms (27.34%), Space: 0B (100%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,4 @@
+/* Write your PL/SQL query statement below */
+select customer_number from Orders
+group by (customer_number) having count(order_number)=(select max(order_num) from
+(select count(order_number) as order_num from Orders group by (customer_number))) ;
