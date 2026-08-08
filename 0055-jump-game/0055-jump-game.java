f22cb857ea29b2169b24224a6a755b48ab3f5d38
Avisikta Nath <168320879+Avisiktaa@users.noreply.github.com>
2026-08-08 20:59:47 +0530

    Time: 2 ms (88.78%), Space: 47.8 MB (63.34%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n=nums.length;
-        int maxid=0;
-        for(int i=0;i<=maxid;i++)
-        {
-            maxid=Math.max(maxid,i+nums[i]);
-            if(maxid>=n-1)
-            return true;
-        }
+      int n=nums.length;
+      int farthest=0;
+
+      for(int i=0;i<n;i++)
+      {
+        if(i>farthest)
         return false;
+
+        farthest=Math.max(farthest,nums[i]+i);
+      }  
+      return true;
     }
 }
