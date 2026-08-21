8731895f04234d8cfa1625a4c7bad159b17c2432
Avisikta Nath <168320879+Avisiktaa@users.noreply.github.com>
2026-08-21 21:12:37 +0530

    Time: 24 ms (76.17%), Space: 48.3 MB (93.92%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,22 +1,5 @@
 class Solution {
 
-    public int count(int[] nums,int k)
-    {
-        int left=0,cnt=0;
-        long sum=0;
-        for(int right=0;right<nums.length;right++)
-        {
-            sum+=nums[right];
-
-            while(sum>k && left<=right)
-            {
-                sum-=nums[left];
-                left++;
-            }
-            cnt+=(right-left+1);
-        }
-        return cnt;
-    }
     public int subarraySum(int[] nums, int k) {
       HashMap<Integer,Integer> map=new HashMap<>();
       int cnt=0,pre=0;
